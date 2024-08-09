e13bf89852570123eb15083a2204c8f86d5028fb
Giulio Riondino <riondino.giulio@gmail.com>
2024-08-08 21:37:29 -0700

    Add equals functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let displayValue = [0],
+    isFirstCharacter = true,
     firstNumber,
     operator,
     secondNumber
@@ -6,6 +7,7 @@ let displayValue = [0],
 const display = document.querySelector('.display')
 const allClearButton = document.querySelector('#ac')
 const signs = document.querySelectorAll('.sign')
+const equalsButton = document.querySelector('#equals')
 const numbers = document.querySelectorAll('.number')
 const pedios = document.querySelector('#period')
 
@@ -42,7 +44,8 @@ allClearButton.addEventListener('click', () => {
 
 numbers.forEach(number => {
     number.addEventListener('click', (e) => {
-        if (displayValue.length === 1 && displayValue[0] == 0) {
+        if (isFirstCharacter) {
+            isFirstCharacter = false
             displayValue[0] = e.currentTarget.textContent
         } else {
             displayValue.push(e.currentTarget.textContent)   
@@ -50,5 +53,22 @@ numbers.forEach(number => {
         display.textContent = displayValue.join('')
     }) 
 })
+
+equalsButton.addEventListener('click', (e => {
+    if (e.currentTarget.id === "equals") {
+        secondNumber = displayValue.join('')
+        operate(firstNumber, secondNumber, operator)
+        isFirstCharacter = true
+    }
+}))
+
+signs.forEach(sign => {
+    sign.addEventListener('click', (e) => {
+        firstNumber = displayValue.join('')
+        operator = e.currentTarget.id
+        isFirstCharacter = true
+    })
+})
+
 
 display.textContent = displayValue.join('')
